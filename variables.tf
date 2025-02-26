55b29613ed63964966c68fdf88420966b539eb63
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2025-02-26 10:34:08 +0000

    k8

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 variable "sg_id" {
   type        = string
-  default     = "sg-0fea5e49e962e81c9"
+  default     = "sg-099a766437c0af022"
   description = "description"
 }
 
 variable "public_subnet_id" {
     type = string
-    default = "subnet-0ea509ad4cba242d7"
+    default = "subnet-0ee1965747130215a"
 }
+
+# allow everything sg and default vpc available subnet id i took 1d region out of 6
